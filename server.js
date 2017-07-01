4fe935736c83f2f7d611c45fabc06265e5a64ddc
Mayank <nitb.scholar@gmail.com>
2017-07-01 20:24:47 +0530

    Added socket.io support

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT, app, express, fs, jsonserver, path, proxy, url;
+var PORT, app, express, fs, jsonserver, path, proxy, url, _;
 
 path = require('path');
 express = require('express');
@@ -6,8 +6,22 @@ jsonserver = require('json-server');
 proxy = require('express-http-proxy');
 fs = require('fs');
 url = require('url');
+_ = require('lodash');
 
 app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var sql = require('mysql');
+
+var mysql = sql.createConnection({
+    user: 'root',
+    password: 'root',
+    database: 'debate',
+    host: 'localhost'
+});
+
+mysql.connect();
+
 PORT = process.env.PORT || 3000;
 process.env.PWD = process.cwd();
 
@@ -15,12 +29,51 @@ app.use('/assets', express["static"](path.join(process.env.PWD, 'assets')));
 app.use('/views', express["static"](path.join(process.env.PWD, 'views')));
 
 app.use('/api', proxy('127.0.0.1:8000', {
-forwardPath: function(req, res) {
-  return '/api' + url.parse(req.url).path;
-}
+    forwardPath: function(req, res) {
+      return '/api' + url.parse(req.url).path;
+    }
 }));
 
-
-app.listen(PORT, function() {
+io.listen(app.listen(PORT, function() {
     return console.log('Listening to ' + PORT);
+}));
+
+// socket stuff
+var socketList = {};
+var userList = {};
+
+io.on('connection', function(socket){
+
+    // ask for authentication
+    socket.emit('auth');
+
+    // initiates the page (authentication callback)
+    socket.on('init', function(data){
+        mysql.query('select * from users where accessToken = ?', [data.token], function(err, result){
+            if(err) console.log('Error', err);
+            result = result[0];
+
+            _.set(socketList, socket.id, result);
+
+            if(_.get(userList, result.uid, false) === false){
+                _.set(userList, result.uid, []);
+            }
+
+            length = _.get(userList, result.uid, []).length;
+            _.set(userList, [result.uid, length], socket.id);
+
+        });
+    });
+
+    // on user disconnects
+    socket.on('disconnect', function(){
+        var userId = _.get(socketList, [socket.id, 'uid'], '-1');
+
+        delete socketList[socket.id];
+        var index = userList[userId].indexOf(socket.id);
+        if(index >=0 ) {
+            userList[userId].splice(index, 1);
+        }
+    });
+
 });
