b898e7b90f1ec19976d5d4b594967d8f22132a8a
LinkedRohit <linkdrohit@gmail.com>
2017-07-02 01:14:15 +0530

    Add facebook login to the app and resolve conflicts of socket.io

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT, app, express, fs, jsonserver, path, proxy, url, _, L, chalk;
+var PORT, app, express, fs, jsonserver, path, proxy, url, _;
 
 path = require('path');
 express = require('express');
@@ -7,16 +7,6 @@ proxy = require('express-http-proxy');
 fs = require('fs');
 url = require('url');
 _ = require('lodash');
-chalk = require('chalk');
-
-L = {
-    info: function(a, b){
-        console.log(chalk.blue('['+a+']'),b);
-    },
-    err: function(a, b){
-        console.log(chalk.red('['+a+']'),b);
-    }
-}
 
 app = express();
 var http = require('http').Server(app);
@@ -51,44 +41,17 @@ io.listen(app.listen(PORT, function() {
 // socket stuff
 var socketList = {};
 var userList = {};
-
-function processPage(socket, path){
-    var page = new url.parse(path, true);
-
-    switch(page.pathname){
-        case '/views/debate.html':
-            L.info('Page', path);
-            debatePageInit(socket, page);
-        break;
-        default:
-            L.error('Page Not Found', path);
-        break;
-    }
-}
-
-function debatePageInit(socket, page){
-    var id = page.query.id;
-    mysql.query('select fullName, postedOn, description from topics ' +
-                'join users on topics.uid = users.uid where tid = ?', [id], function(err, result){
-        if(err) console.log('Error', err);
-        result = result[0];
-        L.info('Question', result.description);
-        socket.emit('debate-ques', result);
-    });
-}
-
+/*
 io.on('connection', function(socket){
 
     // ask for authentication
     socket.emit('auth');
-    L.info(socket.id, 'User Connected');
 
     // initiates the page (authentication callback)
     socket.on('init', function(data){
         mysql.query('select * from users where accessToken = ?', [data.token], function(err, result){
             if(err) console.log('Error', err);
             result = result[0];
-            L.info(result.uid, 'Authenticated');
 
             _.set(socketList, socket.id, result);
 
@@ -99,14 +62,12 @@ io.on('connection', function(socket){
             length = _.get(userList, result.uid, []).length;
             _.set(userList, [result.uid, length], socket.id);
 
-            processPage(socket, data.url);
         });
     });
 
     // on user disconnects
     socket.on('disconnect', function(){
         var userId = _.get(socketList, [socket.id, 'uid'], '-1');
-        L.err(userId, 'User Disconnected');
 
         delete socketList[socket.id];
         var index = userList[userId].indexOf(socket.id);
@@ -115,4 +76,4 @@ io.on('connection', function(socket){
         }
     });
 
-});
+});*/
