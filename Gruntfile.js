9ae80e32d20add9cff7702dfc81baf014724afd6
Mayank <nitb.scholar@gmail.com>
2017-07-17 20:18:48 +0530

    Added live reloading using nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,19 @@ module.exports = function(grunt) {
                     'assets/layout.html': 'views/layout.html'
                 }
             }
+        },
+        exec: {
+            fakeserver: {
+                command: 'nodemon server.js',
+                stdout: true,
+                stderr: true
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('develop', ['exec:fakeserver']);
 }
