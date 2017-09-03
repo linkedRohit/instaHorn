1bbcd2e5c4d5707e626b9cbf1077a972fd382931
Mayank <nitb.scholar@gmail.com>
2017-09-03 23:33:24 +0530

    maran muii ralle

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
                 files: [
                     {
                         expand: true,
-                        cwd: 'view/components/',
+                        cwd: 'views/components/',
                         src: '*',
                         dest: 'build/html/components/'
                     },
